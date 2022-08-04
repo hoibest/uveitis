d895f4af57a0a15305b9913e986224cc3ac16199
hoibest <106015658+hoibest@users.noreply.github.com>
2022-08-04 23:33:22 +1000

    update

---
+++ js/exclusion.js
@@ -0,0 +1,42 @@
+let cmvauEx = 
+`<ol><li>Positive serology for syphilis using a treponemal test
+<li>Evidence of sarcoidosis (either bilateral hilar adenopathy on chest imaging or tissue biopsy demonstrating non-caseating granulomata)
+<li>Aqueous specimen PCR positive for herpes simplex virus or varicella zoster virus</ol>`
+
+let hsauEx = 
+`<ol><li>Concomitant dermatomal/cutaneous varicella zoster virus (unless aqueous specimen PCR positive for herpes simplex virus)
+<li>Positive Serology for syphillis using a treponemal test
+<li>Evidence of sarcoidosis (either bilateral hilar adenopathy on chest imaging or tissue bopsy demonstrating non-caseating granulomata)
+<li>Aqueous specimen PCR positive for cytomegalovirus or varicella zoster virus</ol>`
+
+let vzvauEx = 
+`<ol><li>Positive serology for syphilis using a treponemal test
+<li>Evidence of sarcoidosis (either bilateral hilar adenopathy on chest imaging or tissue biopsy demonstrating non-caseating granulomata)
+<li>Aqueous specimen PCR positive for cytomegalovirus or herpes simplex virus</ol>`
+
+let fusEx = 
+`<ol><li>Positive serology for syphilis using a treponemal test
+<li>Evidence of sarcoidosis (either bilateral hilar adenopathy on chest imaging or tissue biopsy demonstrating non-caseating granulomata)
+<li>Aqueous specimen PCR* positive for cytomegalovirus, herpes simplex virus or varicella
+zoster virus</ol>`
+
+let jiacauEx = 
+`<ol><li>Enthesitis-related arthritis
+<li>Positive serologic test for syphilis using a treponemal test
+<li>Evidence of sarcoidosis (either bilateral hilar adenopathy on chest imaging or tissue biopsy demonstrating non-caseating granulomata) or other granulomatous disease (e.g. familial juvenile systemic granulomatosis)
+<li>Aqueous specimen PCR positive for cytomegalovirus, herpes simplex virus, or varicella
+zoster virus</ol>`
+
+let sauEx = 
+`<ol><li>Positive serology for syphilis using a treponemal test
+<li>Evidence of sarcoidosis (either bilateral hilar adenopathy on chest imaging or tissue biopsy demonstrating non-caseating granulomata)
+<li>Aqueous specimen PCR positive for cytomegalovirus, Herpes simplex virus or Varicella
+zoster virus</ol>`
+
+let tinuEx = 
+`<ol><li>Positive serology for syphilis using a treponemal test
+<li>Evidence of sarcoidosis (either bilateral hilar adenopathy on chest imaging or tissue
+biopsy demonstrating non-caseating granulomata)</ol>`
+
+document.getElementById('exclusion-el').innerHTML = tinuEx;
+
